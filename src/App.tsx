b944481c2f09fbf8072dd66b58c85f7fa72b44e8
thiagoDEVasconcelos <thiagovsousa21@gmail.com>
2025-04-10 19:59:18 -0300

    Add CountDown component and form in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {Container} from "./components/Container";
-import { Heading } from "./components/Heading";
+import { CountDown } from "./components/CountDown";
 import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 import "./styles/global.css"
 import "./styles/theme.css"
@@ -9,7 +10,29 @@ function App() {
   return (
     <>
     <Container><Logo /></Container>
-    <Container><Heading>Menu</Heading></Container>
+    <Container><Menu></Menu></Container>
+    <Container><CountDown/></Container>
+    <Container>
+      <form action="" className="form">
+        <div className="formRow">
+          <label htmlFor="input">task</label>
+          <input type="text" id="input"/>
+        </div>
+
+        <div className="formRow">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+
+        <div className="formRow">
+          <p>Ciclos</p>
+          <p>0 0 0 0 0 0 0</p>
+        </div>
+
+        <div className="formRow">
+          <button>Enviar</button>
+        </div>
+      </form>
+    </Container>
     </>
   )
 }
